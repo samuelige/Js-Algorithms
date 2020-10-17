ca3c3be5ccd64a9ead8924bd4385a04f1d8de17b
samuelige <samuelige.iyanu@gmail.com>
2020-10-17 21:44:49 +0100

    numbers divisible by 2 and 3

--- app.js
+++ app.js
@@ -8,9 +8,17 @@
 
 // 2 write a Javascript function that accept a number and format it to a currency?
 
-var formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'NGN',
-  });
+// var formatter = new Intl.NumberFormat('en-US', {
+//     style: 'currency',
+//     currency: 'NGN',
+//   });
   
-  console.log(formatter.format(2000));
+//   console.log(formatter.format(2000));
+
+// 3 write a function that accept an array of numbers and output an array of numbers divisible by 2 and 3 ?
+
+const arraryofNum = (arr) => {
+    return  arr.filter(item => item % 2 === 0 && item % 3 === 0);
+}
+
+console.log(arraryofNum([6, 12, 18, 24, 30, 36, 42, 48, 54, 60, 66, 72, 78, 84, 90, 96, 102, 108, 114, 120, 1, 5, 7, 11, 13,17, 19,23, 25, 29, 31]));
