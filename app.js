72ac9c5f6f9c0d291df391423a24d62384401978
samuelige <samuelige.iyanu@gmail.com>
2020-10-17 20:47:28 +0100

    numberformat to currency

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-// write a Javascript function that prints 'HELLO WORLD'
 
-const hello = () => {
-    return `Hello World!!!`;
-}
+// 1 write a Javascript function that prints 'HELLO WORLD'?
 
-console.log(hello());
+// const hello = () => {
+//     return `Hello World!!!`;
+// }
+// console.log(hello());
+
+// 2 write a Javascript function that accept a number and format it to a currency?
+
+var formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'NGN',
+  });
+  
+  console.log(formatter.format(2000));
